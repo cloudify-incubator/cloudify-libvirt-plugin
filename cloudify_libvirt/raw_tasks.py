a82b3109db400ea60461c53d8eff545ef466f232
Denis Pauk <pauk.denis@gmail.com>
2016-10-06 18:21:27 +0300

    stub implementation

---
+++ cloudify_libvirt/raw_tasks.py
@@ -0,0 +1,43 @@
+########
+# Copyright (c) 2016 GigaSpaces Technologies Ltd. All rights reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+#    * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    * See the License for the specific language governing permissions and
+#    * limitations under the License.
+
+
+from cloudify import ctx
+from cloudify.decorators import operation
+
+
+@operation
+def create(**kwargs):
+    ctx.logger.info("create")
+
+
+@operation
+def configure(**kwargs):
+    ctx.logger.info("configure")
+
+
+@operation
+def start(**kwargs):
+    ctx.logger.info("start")
+
+
+@operation
+def stop(**kwargs):
+    ctx.logger.info("stop")
+
+
+@operation
+def delete(some_property, **kwargs):
+    ctx.logger.info("delete")
