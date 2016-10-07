ca6a175ca81b0cb0a89df47f34dd3dfc033c57bb
Denis Pauk <pauk.denis@gmail.com>
2016-10-07 12:06:02 +0300

    stub implementation

---
+++ setup.py
@@ -0,0 +1,29 @@
+# Copyright (c) 2016 GigaSpaces Technologies Ltd. All rights reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from setuptools import setup
+
+setup(
+    name='cloudify-libvirt-plugin',
+    version='0.1',
+    description='support netconf',
+    author='Denis Pauk',
+    author_email='pauk.denis@gmail.com',
+    license='LICENSE',
+    packages=['cloudify_libvirt'],
+    install_requires=[
+        'cloudify-plugins-common>=3.3',
+        'lxml',
+        'libvirt-python',
+    ],
+)
