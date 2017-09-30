f13598881c9415e8c498be5fc1eeef64ff0bb8e3
Denis Pauk <pauk.denis@gmail.com>
2017-09-30 14:56:34 +0300

    change download script

--- blueprint_example/scripts/download_vm.sh
+++ blueprint_example/scripts/download_vm.sh
@@ -1,4 +1,5 @@
-ctx logger info "Download base image"
-LANG=C wget -cv wget https://cloud-images.ubuntu.com/trusty/20170919/trusty-server-cloudimg-amd64-disk1.img
+ctx logger info "Download base image ${DISK}"
 
-ctx instance runtime-properties vm_image "`pwd`/trusty-server-cloudimg-amd64-disk1.img"
+LANG=C wget -cv wget "https://cloud-images.ubuntu.com/trusty/20170919/${DISK}"
+
+ctx instance runtime-properties vm_image "`pwd`/${DISK}"
