360ed2e5188fafb704f9da091c7d8101aa3e6d68
Denis Pauk <pauk.denis@gmail.com>
2018-04-26 19:24:07 +0300

    small fixes fro cluster blueprint

--- examples/cluster/libvirt_install.sh
+++ examples/cluster/libvirt_install.sh
@@ -1,3 +1,9 @@
 sudo yum install -y -q epel-release
 sudo yum install -y -q qemu-kvm libvirt wget qemu-system-x86 genisoimage deltarpm iptables
+
+sudo service libvirtd restart
+sudo groupadd libvirt
+sudo usermod --append --groups kvm `whoami`
+sudo usermod --append --groups libvirt `whoami`
+
 sudo service libvirtd restart
