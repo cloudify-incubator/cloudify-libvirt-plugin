dc6604372a8db76e16f9529b4d2d116e4a45e508
EarthmanT <trammell@cloudify.co>
2022-07-19 15:08:50 -0400

    bump libvirt version for rhel8

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     install_requires=[
         'cloudify-common>=4.5.0',
         # libvirt-6.0 requires python3
-        'libvirt-python==7.10',
+        'libvirt-python==8.5.0',
         # cdrom create code
         "cloudify-utilities-plugins-sdk>=0.0.27",
     ],
