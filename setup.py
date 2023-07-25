f3fe54b2880d1020d98c0ce413f3d7f937f4a55b
Ahmad Musa <ahmad@cloudify.co>
2023-07-25 11:53:49 +0300

    add-passthrough-support

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from setuptools import setup
 
 setup(
     name='cloudify-libvirt-plugin',
-    version='0.9.3',
+    version='0.9.4',
     description='support libvirt',
     author='Cloudify',
     author_email='hello@getcloudify.org',
